097b2ba52d8989cb3e202b100800dfcdf3031ded
Aaron Alphonsus <aaron.alphonsus@gmail.com>
2016-07-28 23:11:34 -0600

    Adds simple tf-idf implementation for single-word queries.

--- javacs-lab12/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab12/src/com/flatironschool/javacs/TermCounter.java
@@ -17,136 +17,144 @@ import org.jsoup.select.Elements;
  *
  */
 public class TermCounter {
-	
-	private Map<String, Integer> map;
-	private String label;
-	
-	public TermCounter(String label) {
-		this.label = label;
-		this.map = new HashMap<String, Integer>();
-	}
-	
-	public String getLabel() {
-		return label;
-	}
-	
-	/**
-	 * Returns the total of all counts.
-	 * 
-	 * @return
-	 */
-	public int size() {
-		int total = 0;
-		for (Integer value: map.values()) {
-			total += value;
-		}
-		return total;
-	}
 
-	/**
-	 * Takes a collection of Elements and counts their words.
-	 * 
-	 * @param paragraphs
-	 */
-	public void processElements(Elements paragraphs) {
-		for (Node node: paragraphs) {
-			processTree(node);
-		}
-	}
-	
-	/**
-	 * Finds TextNodes in a DOM tree and counts their words.
-	 * 
-	 * @param root
-	 */
-	public void processTree(Node root) {
-		// NOTE: we could use select to find the TextNodes, but since
-		// we already have a tree iterator, let's use it.
-		for (Node node: new WikiNodeIterable(root)) {
-			if (node instanceof TextNode) {
-				processText(((TextNode) node).text());
-			}
-		}
-	}
+   private Map<String, Double> map;
+   private String label;
 
-	/**
-	 * Splits `text` into words and counts them.
-	 * 
-	 * @param text  The text to process.
-	 */
-	public void processText(String text) {
-		// replace punctuation with spaces, convert to lower case, and split on whitespace
-		String[] array = text.replaceAll("\\pP", " ").toLowerCase().split("\\s+");
-		
-		for (int i=0; i<array.length; i++) {
-			String term = array[i];
-			incrementTermCount(term);
-		}
-	}
+   public TermCounter(String label) {
+      this.label = label;
+      this.map = new HashMap<String, Double>();
+   }
 
-	/**
-	 * Increments the counter associated with `term`.
-	 * 
-	 * @param term
-	 */
-	public void incrementTermCount(String term) {
-		// System.out.println(term);
-		put(term, get(term) + 1);
-	}
+   public String getLabel() {
+      return label;
+   }
 
-	/**
-	 * Adds a term to the map with a given count.
-	 * 
-	 * @param term
-	 * @param count
-	 */
-	public void put(String term, int count) {
-		map.put(term, count);
-	}
+   /**
+    * Takes a collection of Elements and counts their words.
+    * 
+    * @param paragraphs
+    */
+   public void processElements(Elements paragraphs) {
+      for (Node node: paragraphs) {
+         processTree(node);
+      }
+      tf();
+   }
 
-	/**
-	 * Returns the count associated with this term, or 0 if it is unseen.
-	 * 
-	 * @param term
-	 * @return
-	 */
-	public Integer get(String term) {
-		Integer count = map.get(term);
-		return count == null ? 0 : count;
-	}
+   /**
+    * Finds TextNodes in a DOM tree and counts their words.
+    * 
+    * @param root
+    */
+   public void processTree(Node root) {
+      // NOTE: we could use select to find the TextNodes, but since
+      // we already have a tree iterator, let's use it.
+      for (Node node: new WikiNodeIterable(root)) {
+         if (node instanceof TextNode) {
+            processText(((TextNode) node).text());
+         }
+      }
+   }
 
-	/**
-	 * Returns the set of terms that have been counted.
-	 * 
-	 * @return
-	 */
-	public Set<String> keySet() {
-		return map.keySet();
-	}
-	
-	/**
-	 * Print the terms and their counts in arbitrary order.
-	 */
-	public void printCounts() {
-		for (String key: keySet()) {
-			Integer count = get(key);
-			System.out.println(key + ", " + count);
-		}
-		System.out.println("Total of all counts = " + size());
-	}
+   /**
+    * Splits `text` into words and counts them.
+    * 
+    * @param text  The text to process.
+    */
+   public void processText(String text) {
+      // replace punctuation with spaces, convert to lower case, and split on whitespace
+      String[] array = text.replaceAll("\\pP", " ").toLowerCase().split("\\s+");
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
-	public static void main(String[] args) throws IOException {
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		
-		WikiFetcher wf = new WikiFetcher();
-		Elements paragraphs = wf.fetchWikipedia(url);
-		
-		TermCounter counter = new TermCounter(url.toString());
-		counter.processElements(paragraphs);
-		counter.printCounts();
-	}
+      for (int i=0; i<array.length; i++) {
+         String term = array[i];
+         incrementTermCount(term);
+      }
+   }
+
+   /**
+    * Increments the counter associated with `term`.
+    * 
+    * @param term
+    */
+   public void incrementTermCount(String term) {
+      // System.out.println(term);
+      put(term, get(term) + 1);
+   }
+
+   /**
+    * Adds a term to the map with a given count.
+    * 
+    * @param term
+    * @param count
+    */
+   public void put(String term, double count) {
+      map.put(term, count);
+   }
+
+   /**
+    * Returns the count associated with this term, or 0 if it is unseen.
+    * 
+    * @param term
+    * @return
+    */
+   public Double get(String term) {
+      Double count = map.get(term);
+      return count == null ? 0 : count;
+   }
+
+   /**
+    * Print the terms and their counts in arbitrary order.
+    */
+   public void printTF() {
+      for (String key: keySet()) {
+         Double count = get(key);
+         System.out.println(key + ", " + count);
+      }
+   }
+
+   /**
+    * Returns the set of terms that have been counted.
+    * 
+    * @return
+    */
+   public Set<String> keySet() {
+      return map.keySet();
+   }
+
+   /**
+    * Calculates tf and puts in map
+    * 
+    * @return
+    */
+   public void tf() {
+
+      double total = 0;
+      for (Double value: map.values()) {
+         total += value;
+      }
+      // System.out.println ( total );
+      
+      for ( Map.Entry<String, Double> entry: map.entrySet() ) {
+         // System.out.println( entry );
+         //System.out.println( entry.getValue()/total );
+         
+         put( entry.getKey(), entry.getValue()/total );
+      }      
+   }
+
+   /**
+    * @param args
+    * @throws IOException 
+    */
+   public static void main(String[] args) throws IOException {
+      String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+
+      WikiFetcher wf = new WikiFetcher();
+      Elements paragraphs = wf.fetchWikipedia(url);
+
+      TermCounter counter = new TermCounter(url.toString());
+      counter.processElements(paragraphs);
+      counter.printTF();
+   }
 }
